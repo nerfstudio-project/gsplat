bcbed0b95f10294ea18f2aad960de9b75a7558d4
maturk <matiasturkulainen@gmail.com>
2023-09-17 22:14:38 +0300

    add ref code to run on host

--- csrc/check_serial_forward.cu
+++ csrc/check_serial_forward.cu
@@ -13,17 +13,6 @@
 
 float random_float() { return (float)std::rand() / RAND_MAX; }
 
-__global__ void cov2d_kernel_entry(const float3& mean, float fx, float fy, float tan_fovx, float tan_fovy, const float* cov3d, const float* viewmat, float3* ref_res_d, const float num_points)
-{
-    // entry point to call reference device code
-    auto idx = cg::this_grid().thread_rank();
-    if (idx >= num_points)
-		return;
-
-    float3 res = computeCov2D(mean, fx, fy, tan_fovx, tan_fovy, cov3d, viewmat);
-    ref_res_d[idx] = res;
-    
-}
 void compare_cov2d_forward(){
     int num_points = 1;
     float fx = 1;
@@ -55,21 +44,8 @@ void compare_cov2d_forward(){
     std::cout<<"diff rast cov2d: "<<diff_rast_cov2d.x<<" "<<diff_rast_cov2d.y<<" "<<diff_rast_cov2d.z<<std::endl; 
     
     // ref rast
-    float3 *ref_res_d;// = new float3[num_points];
-    cudaMalloc((void **)&ref_res_d, num_points * sizeof(float3));
-
-    cov2d_kernel_entry<<<1,1>>>(mean, fx, fy, tan_fovx, tan_fovy, cov3d, viewmatColumnMajor, ref_res_d, num_points);
-    cudaDeviceSynchronize();
-
-    float3 ref_rast_cov2d[num_points];
-    cudaMemcpy(
-        ref_rast_cov2d, ref_res_d, num_points * sizeof(float3), cudaMemcpyDeviceToHost
-    );
-
-    cudaFree(ref_res_d);
-
-    // Why is the output not correct :(
-    std::cout<<"ref rast cov2d:  "<<ref_rast_cov2d[0].x<<" "<<ref_rast_cov2d[0].y<<" "<<ref_rast_cov2d[0].z<<std::endl;
+    float3 res = computeCov2D(mean, fx, fy, tan_fovx, tan_fovy, cov3d, viewmatColumnMajor);
+    std::cout<<"ref rast cov2d:  "<<res.x<<" "<<res.y<<" "<<res.z<<std::endl;
 }
 
 int main(){
