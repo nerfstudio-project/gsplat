d2f0676f4a8fa1e45f440031bc34a743894d5338
maturk <matiasturkulainen@gmail.com>
2023-09-17 23:01:07 +0300

    currently NOT matching: cov3d from scale and rot checks

--- csrc/check_serial_forward.cu
+++ csrc/check_serial_forward.cu
@@ -13,7 +13,19 @@
 
 float random_float() { return (float)std::rand() / RAND_MAX; }
 
+float4 random_quat() {
+    float u = random_float();
+    float v = random_float();
+    float w = random_float();
+    return {
+        sqrt(1.f - u) * sin(2.f * (float)M_PI * v),
+        sqrt(1.f - u) * cos(2.f * (float)M_PI * v),
+        sqrt(u) * sin(2.f * (float)M_PI * w),
+        sqrt(u) * cos(2.f * (float)M_PI * w)};
+}
+
 void compare_cov2d_forward(){
+    // TODO: test with more than one point and varying cov3d/viewmat
     int num_points = 1;
     float fx = 1;
     float fy = 1;
@@ -48,6 +60,28 @@ void compare_cov2d_forward(){
     std::cout<<"ref rast cov2d:  "<<res.x<<" "<<res.y<<" "<<res.z<<std::endl;
 }
 
+void compare_scale_rot_to_cov3d(){
+    // TODO: make it work with more than one point
+    int num_points = 1;
+    float3 scale = {random_float(), random_float(), random_float()};
+    float glob_scale = random_float();
+
+    float4 quat = random_quat();
+
+    float* cov3d = new float[6 * num_points];
+
+    // diff rast
+    scale_rot_to_cov3d(scale, glob_scale, quat, cov3d);   
+    std::cout<<cov3d[0]<<" "<<cov3d[1]<<" "<<cov3d[2]<<std::endl;
+
+    // ref rast
+    float* ref_cov3d = new float[6 * num_points];
+    computeCov3D(glm::vec3({scale.x,scale.y,scale.z}), glob_scale, glm::vec4({quat.x, quat.y, quat.z, quat.w}), ref_cov3d);
+    std::cout<<ref_cov3d[0]<<" "<<ref_cov3d[1]<<" "<<ref_cov3d[2]<<std::endl;
+}
+
+
 int main(){
     compare_cov2d_forward();
+    compare_scale_rot_to_cov3d();
 }
