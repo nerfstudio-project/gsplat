ed61a38fafa1066aa16e8e503748df8fcd2630d1
Ruilong Li <ruilongli94@gmail.com>
2023-10-04 16:16:30 +0000

    hash works

--- examples/simple_trainer_hash.py
+++ examples/simple_trainer_hash.py
@@ -1,6 +1,5 @@
 import math
 import os
-import random
 from pathlib import Path
 from typing import Optional
 
@@ -12,6 +11,70 @@ from torch import Tensor, optim
 
 from diff_rast.project_gaussians import ProjectGaussians
 from diff_rast.rasterize import RasterizeGaussians
+
+
+class GSHashEncoding(torch.nn.Module):
+    """Hash Encoding designed for GS."""
+
+    def __init__(
+        self,
+        output_dim: int,
+        # hashmap_size_list: int = [64, 128, 256, 512, 1024],
+        # hashmap_size_list: int = [16],
+        hashmap_size_list: int = [16, 32],
+        hashmap_dim: int = 4,
+        reso: int = 2048,
+    ):
+        super().__init__()
+        self.hashmap_size_list = hashmap_size_list
+        self.hashmap_dim = hashmap_dim
+        self.reso = reso
+
+        codes = []
+        sizes = []
+        for hashmap_size in hashmap_size_list:
+            if reso <= hashmap_size:
+                code = torch.randn((reso, hashmap_dim))
+                size = reso
+            else:
+                code = torch.randn((hashmap_size, hashmap_dim))
+                size = hashmap_size
+                map = torch.randint(0, hashmap_size, size=(reso, hashmap_dim))
+                self.register_buffer("map_" + str(hashmap_size), map)
+
+            torch.nn.init.uniform_(code, -1.0, 1.0)
+            codes.append(code)
+            sizes.append(size)
+        codes = torch.cat(codes, dim=0)
+
+        self.codes = torch.nn.Parameter(codes, requires_grad=True)
+        self.sizes = sizes
+
+        self.mlp_head = torch.nn.Sequential(
+            torch.nn.Linear(
+                len(hashmap_size_list) * hashmap_dim, output_dim, bias=False
+            ),
+            # torch.nn.ReLU(),
+            # torch.nn.Linear(64, output_dim),
+        )
+
+    def forward(self):
+        codes = torch.split(self.codes, self.sizes, dim=0)
+
+        outs = []
+        for hashmap_size, code in zip(self.hashmap_size_list, codes):
+            if self.reso <= hashmap_size:
+                outs.append(code)
+            else:
+                map = getattr(self, "map_" + str(hashmap_size))
+                out = torch.stack(
+                    [code[map[:, i], i] for i in range(self.hashmap_dim)],
+                    dim=-1,
+                )
+                outs.append(out)
+        outs = torch.cat(outs, dim=1)
+        outs = self.mlp_head(outs)
+        return outs.to(self.codes)
 
 
 class SimpleTrainer:
@@ -42,42 +105,11 @@ class SimpleTrainer:
 
     def _init_gaussians(self):
         """Random gaussians"""
-        self.means = torch.empty((self.num_points, 3), device=self.device)
-        self.scales = torch.empty((self.num_points, 3), device=self.device)
-        self.quats = torch.empty((self.num_points, 4), device=self.device)
-        self.rgbs = torch.ones((self.num_points, 3), device=self.device)
-        self.opacities = torch.ones((self.num_points, 1), device=self.device)
-        bd = 2
-        for i in range(self.num_points):
-            self.means[i] = torch.tensor(
-                [
-                    bd * (random.random() - 0.5),
-                    bd * (random.random() - 0.5),
-                    bd * (random.random() - 0.5),
-                ],
-                device=self.device,
-            )
-            self.scales[i] = torch.tensor(
-                [random.random(), random.random(), random.random()],
-                device=self.device,
-            )
-            self.rgbs[i] = torch.tensor(
-                [random.random(), random.random(), random.random()],
-                device=self.device,
-            )
-            u = random.random()
-            v = random.random()
-            w = random.random()
-            self.quats[i] = torch.tensor(
-                [
-                    math.sqrt(1.0 - u) * math.sin(2.0 * math.pi * v),
-                    math.sqrt(1.0 - u) * math.cos(2.0 * math.pi * v),
-                    math.sqrt(u) * math.sin(2.0 * math.pi * w),
-                    math.sqrt(u) * math.cos(2.0 * math.pi * w),
-                ],
-                device=self.device,
-            )
-
+        self.means = GSHashEncoding(3, reso=self.num_points).to(self.device)
+        self.scales = GSHashEncoding(3, reso=self.num_points).to(self.device)
+        self.rgbs = GSHashEncoding(3, reso=self.num_points).to(self.device)
+        self.quats = GSHashEncoding(4, reso=self.num_points).to(self.device)
+        self.opacities = GSHashEncoding(1, reso=self.num_points).to(self.device)
         self.viewmat = torch.tensor(
             [
                 [1.0, 0.0, 0.0, 0.0],
@@ -87,19 +119,20 @@ class SimpleTrainer:
             ],
             device=self.device,
         )
-        self.means.requires_grad = True
-        self.scales.requires_grad = True
-        self.quats.requires_grad = True
-        self.rgbs.requires_grad = True
-        self.opacities.requires_grad = True
-        self.viewmat.requires_grad = False
 
     def train(
         self, iterations: int = 1000, lr: float = 0.01, save_imgs: bool = True
     ):
         optimizer = optim.Adam(
-            [self.rgbs, self.means, self.scales, self.opacities, self.quats], lr
+            [
+                {"params": self.means.parameters(), "lr": lr},
+                {"params": self.scales.parameters(), "lr": lr},
+                {"params": self.rgbs.parameters(), "lr": lr},
+                {"params": self.quats.parameters(), "lr": lr},
+                {"params": self.opacities.parameters(), "lr": lr},
+            ]
         )
+
         scheduler = torch.optim.lr_scheduler.ChainedScheduler(
             [
                 torch.optim.lr_scheduler.LinearLR(
@@ -117,7 +150,19 @@ class SimpleTrainer:
         )
         mse_loss = torch.nn.MSELoss()
         frames = []
+
+        with torch.no_grad():
+            means = self.means()
+            means_min = means.min(dim=0).values
+            means_max = means.max(dim=0).values
+
         for iter in range(iterations):
+            means = self.means()
+            # means = (means - means_min) / (means_max - means_min) * 2 - 1
+            scales = self.scales()
+            rgbs = self.rgbs()
+            quats = self.quats()
+            opacities = self.opacities()
             (
                 xys,
                 depths,
@@ -126,10 +171,10 @@ class SimpleTrainer:
                 num_tiles_hit,
                 cov3d,
             ) = ProjectGaussians.apply(
-                self.means,
-                self.scales,
+                means,
+                scales,
                 1,
-                self.quats,
+                quats,
                 self.viewmat,
                 self.viewmat,
                 self.focal,
@@ -145,8 +190,8 @@ class SimpleTrainer:
                 radii,
                 conics,
                 num_tiles_hit,
-                torch.sigmoid(self.rgbs),
-                torch.sigmoid(self.opacities),
+                torch.sigmoid(rgbs),
+                torch.sigmoid(opacities),
                 self.H,
                 self.W,
             )
@@ -165,13 +210,36 @@ class SimpleTrainer:
             # save them as a gif with PIL
             frames = [Image.fromarray(frame) for frame in frames]
             frames[0].save(
-                os.getcwd() + "/renders/training.gif",
+                os.getcwd() + "/training.gif",
                 save_all=True,
                 append_images=frames[1:],
                 optimize=False,
                 duration=5,
                 loop=0,
             )
+        n_params = 0
+        n_params += sum(p.numel() for p in self.means.parameters())
+        n_params += sum(p.numel() for p in self.scales.parameters())
+        n_params += sum(p.numel() for p in self.rgbs.parameters())
+        n_params += sum(p.numel() for p in self.quats.parameters())
+        n_params += sum(p.numel() for p in self.opacities.parameters())
+        print("Trainable parameters:", n_params)
+
+
+# [10, 3]
+trainable_coords = torch.randn((10, 3))
+hash_mapping_x = torch.randint(low=0, high=10, size=(1000,))
+hash_mapping_y = torch.randint(low=0, high=10, size=(1000,))
+hash_mapping_z = torch.randint(low=0, high=10, size=(1000,))
+# [1000, 3]
+coords = torch.stack(
+    [
+        trainable_coords[hash_mapping_x, 0],
+        trainable_coords[hash_mapping_y, 1],
+        trainable_coords[hash_mapping_z, 2],
+    ],
+    dim=1,
+)
 
 
 def image_path_to_tensor(image_path: Path):
