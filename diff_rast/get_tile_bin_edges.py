2e35e1da5ca1198153ad7445c92b6d52d47fa8dc
maturk <matiasturkulainen@gmail.com>
2023-10-05 11:13:04 +0300

    format

--- diff_rast/get_tile_bin_edges.py
+++ diff_rast/get_tile_bin_edges.py
@@ -11,7 +11,7 @@ import diff_rast.cuda as _C
 
 class GetTileBinEdges(Function):
     """Function to map sorted intersection IDs to tile bins which give the range of unique gaussian IDs belonging to each tile.
-    
+
     Expects that intersection IDs are sorted by increasing tile ID.
 
     Indexing into tile_bins[tile_idx] returns the range (lower,upper) of gaussian IDs that hit tile_idx.
@@ -19,7 +19,7 @@ class GetTileBinEdges(Function):
         Args:
             num_intersects (int): total number of gaussian intersects.
             isect_ids_sorted (Tensor): sorted unique IDs for each gaussian in the form (tile | depth id).
-        
+
         Returns:
             tile_bins (Tensor): range of gaussians IDs hit per tile.
     """
@@ -31,7 +31,6 @@ class GetTileBinEdges(Function):
 
         tile_bins = _C.get_tile_bin_edges(num_intersects, isect_ids_sorted)
         return tile_bins
-    
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
