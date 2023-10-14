82da7f0690dbf54145e7d7b4833a90232f811919
Vickie Ye <vye1116@gmail.com>
2023-10-14 00:02:24 +0000

    moving experimental into separate branch

--- gsplat/project_gaussians.py
+++ gsplat/project_gaussians.py
@@ -119,13 +119,7 @@ class ProjectGaussians(Function):
             conics,
         ) = ctx.saved_tensors
 
-        (
-            v_cov2d,
-            v_cov3d,
-            v_mean3d,
-            v_scale,
-            v_quat,
-        ) = _C.project_gaussians_backward(
+        (v_cov2d, v_cov3d, v_mean3d, v_scale, v_quat) = _C.project_gaussians_backward(
             ctx.num_points,
             means3d,
             scales,
