4c3717ce1dfe9095f09cb27f69cf8ef693c0d2ef
FantasticOven2 <0z5e1n3g@berkeley.edu>
2024-06-15 08:11:22 -0700

    variable num points for 2D example

--- examples/2d_example.py
+++ examples/2d_example.py
@@ -15,10 +15,10 @@ class SimpleTrainer:
     
     def __init__(
         self,
-        num_points: int = 8,
+        num_points_per_axis: int = 8,
     ):
         self.device = torch.device("cuda:0")
-        self.num_points = 8
+        self.num_points_per_axis = num_points_per_axis
         
         fov_x = math.pi / 2.0
         self.H, self.W = 256, 256
@@ -29,15 +29,16 @@ class SimpleTrainer:
     
     def _init_gaussians(self):
         length = 0.4
-        x = np.linspace(-1, 1, self.num_points)
-        y = np.linspace(-1, 1, self.num_points)
+        x = np.linspace(-1, 1, self.num_points_per_axis)
+        y = np.linspace(-1, 1, self.num_points_per_axis)
         x, y = np.meshgrid(x, y)
         means3D = torch.from_numpy(np.stack([x, y, np.ones_like(x)], axis=-1).reshape(-1, 3)).cuda().float()
         quats = torch.zeros(1, 4).repeat(len(means3D), 1).cuda()
         quats[..., 0] = 1.
-        scale = 0.6 / (self.num_points - 1)
+        scale = 0.6 / (self.num_points_per_axis - 1)
         scales = torch.zeros(1, 3).repeat(len(means3D), 1).fill_(scale).cuda()
-        colors = matplotlib.colormaps['Accent'](np.random.randint(1, 64, 64) / 64)[..., :3]
+        num_points = self.num_points_per_axis ** 2
+        colors = matplotlib.colormaps['Accent'](np.random.randint(1, num_points, num_points) / num_points)[..., :3]
         colors = torch.from_numpy(colors).cuda()
         opacity = torch.ones_like(means3D[:, 0])
         
@@ -109,12 +110,12 @@ class SimpleTrainer:
 def main(
     height: int = 256,
     width: int = 256,
-    num_points: int = 8,
+    num_points_per_axis: int = 8,
     save_imgs: bool = True,
     img_path: Optional[Path] = None,
     model_type: Literal["3dgs", "2dgs"] = "3dgs",
 ) -> None:
-    trainer = SimpleTrainer(num_points=num_points)
+    trainer = SimpleTrainer(num_points_per_axis=num_points_per_axis)
     trainer.render(
         model_type=model_type,
     )
