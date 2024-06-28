2119fefe10d646b10fb481afaa6949ae32bb8a12
Jeffrey Hu <hujh14@gmail.com>
2024-06-28 10:48:39 -0700

    cuda stream

--- gsplat/cuda/csrc/relocation.cu
+++ gsplat/cuda/csrc/relocation.cu
@@ -2,7 +2,7 @@
 
 // Equation (9) in "3D Gaussian Splatting as Markov Chain Monte Carlo"
 __global__ void compute_relocation_kernel(
-    int P,
+    int N,
     float *opacities,
     float *scales,
     int *ratios,
@@ -12,7 +12,7 @@ __global__ void compute_relocation_kernel(
     float *new_scales
 ) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx >= P)
+    if (idx >= N)
         return;
 
     int n_idx = ratios[idx];
@@ -50,20 +50,19 @@ std::tuple<torch::Tensor, torch::Tensor> compute_relocation_tensor(
     torch::Tensor new_opacities = torch::empty_like(opacities);
     torch::Tensor new_scales = torch::empty_like(scales);
 
-    const int P = opacities.size(0);
-    assert(P != 0);
-    int num_blocks = (P + 255) / 256;
-    dim3 block(256, 1, 1);
-    dim3 grid(num_blocks, 1, 1);
-    compute_relocation_kernel<<<grid, block>>>(
-        P,
-        opacities.data_ptr<float>(),
-        scales.data_ptr<float>(),
-        ratios.data_ptr<int>(),
-        binoms.data_ptr<float>(),
-        n_max,
-        new_opacities.data_ptr<float>(),
-        new_scales.data_ptr<float>()
-    );
+    uint32_t N = opacities.size(0);
+    if (N) {
+        at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
+        compute_relocation_kernel<<<(N + N_THREADS - 1) / N_THREADS, N_THREADS, 0, stream>>>(
+            N,
+            opacities.data_ptr<float>(),
+            scales.data_ptr<float>(),
+            ratios.data_ptr<int>(),
+            binoms.data_ptr<float>(),
+            n_max,
+            new_opacities.data_ptr<float>(),
+            new_scales.data_ptr<float>()
+        );
+    }
     return std::make_tuple(new_opacities, new_scales);
 }
