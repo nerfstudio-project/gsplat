a6063ee805758ed7d4022b2b10d036262c0ab157
Francis Williams <francis@fwilliams.info>
2024-07-04 11:52:50 -0400

    template compute_sh

--- gsplat/cuda/csrc/compute_sh_fwd.cu
+++ gsplat/cuda/csrc/compute_sh_fwd.cu
@@ -7,12 +7,13 @@
 namespace cg = cooperative_groups;
 
 
+template <typename T>
 __global__ void compute_sh_fwd_kernel(const uint32_t N, const uint32_t K,
                                       const uint32_t degrees_to_use,
-                                      const float3 *__restrict__ dirs,  // [N, 3]
-                                      const float *__restrict__ coeffs, // [N, K, 3]
+                                      const typename Float3<T>::type *__restrict__ dirs,  // [N, 3]
+                                      const T *__restrict__ coeffs, // [N, K, 3]
                                       const bool *__restrict__ masks,   // [N]
-                                      float *__restrict__ colors        // [N, 3]
+                                      T *__restrict__ colors        // [N, 3]
 ) {
     // parallelize over N * 3
     uint32_t idx = cg::this_grid().thread_rank();
@@ -47,7 +48,7 @@ torch::Tensor compute_sh_fwd_tensor(const uint32_t degrees_to_use,
     torch::Tensor colors = torch::empty_like(dirs); // [..., 3]
     // parallelize over N * 3
     if (N) {
-        compute_sh_fwd_kernel<<<(N * 3 + N_THREADS - 1) / N_THREADS, N_THREADS>>>(
+        compute_sh_fwd_kernel<float><<<(N * 3 + N_THREADS - 1) / N_THREADS, N_THREADS>>>(
             N, K, degrees_to_use, (float3 *)dirs.data_ptr<float>(),
             coeffs.data_ptr<float>(),
             masks.has_value() ? masks.value().data_ptr<bool>() : nullptr,
