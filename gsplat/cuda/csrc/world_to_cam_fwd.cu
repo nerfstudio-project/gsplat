8605aac95f425d732062b22604133d2a8318e50b
Francis Williams <francis@fwilliams.info>
2024-07-07 13:24:29 -0400

    world to cam fwd

--- gsplat/cuda/csrc/world_to_cam_fwd.cu
+++ gsplat/cuda/csrc/world_to_cam_fwd.cu
@@ -22,8 +22,12 @@ __global__ void world_to_cam_fwd_kernel(const uint32_t C, const uint32_t N,
                                         const T *__restrict__ viewmats, // [C, 4, 4]
                                         T *__restrict__ means_c,        // [C, N, 3]
                                         T *__restrict__ covars_c // [C, N, 3, 3]
-) { // parallelize over C * N.
-    uint32_t idx = cg::this_grid().thread_rank();
+) { 
+    // For now we'll upcast float16 and bfloat16 to float32
+    using OpT = typename OpType<T>::type;
+    
+    // parallelize over C * N.
+    const uint32_t idx = cg::this_grid().thread_rank();
     if (idx >= C * N) {
         return;
     }
@@ -36,32 +40,34 @@ __global__ void world_to_cam_fwd_kernel(const uint32_t C, const uint32_t N,
     viewmats += cid * 16;
 
     // glm is column-major but input is row-major
-    mat3<T> R = mat3<T>(viewmats[0], viewmats[4], viewmats[8], // 1st column
-                        viewmats[1], viewmats[5], viewmats[9], // 2nd column
-                        viewmats[2], viewmats[6], viewmats[10] // 3rd column
+    const mat3<OpT> R = mat3<OpT>(viewmats[0], viewmats[4], viewmats[8], // 1st column
+                                  viewmats[1], viewmats[5], viewmats[9], // 2nd column
+                                  viewmats[2], viewmats[6], viewmats[10] // 3rd column
     );
-    vec3<T> t = vec3<T>(viewmats[3], viewmats[7], viewmats[11]);
+    const vec3<OpT> t = vec3<OpT>(viewmats[3], viewmats[7], viewmats[11]);
 
     if (means_c != nullptr) {
-        vec3<T> mean_c;
-        pos_world_to_cam(R, t, glm::make_vec3(means), mean_c);
+        vec3<OpT> mean_c;
+        const vec3<OpT> mean = glm::make_vec3(means);
+        pos_world_to_cam(R, t, mean, mean_c);
         means_c += idx * 3;
         PRAGMA_UNROLL
         for (uint32_t i = 0; i < 3; i++) { // rows
-            means_c[i] = mean_c[i];
+            means_c[i] = T(mean_c[i]);
         }
     }
 
     // write to outputs: glm is column-major but we want row-major
     if (covars_c != nullptr) {
-        mat3<T> covar_c;
-        covar_world_to_cam(R, glm::make_mat3(covars), covar_c);
+        mat3<OpT> covar_c;
+        const mat3<OpT> covar = glm::make_mat3(covars);
+        covar_world_to_cam<OpT>(R, covar, covar_c);
         covars_c += idx * 9;
         PRAGMA_UNROLL
         for (uint32_t i = 0; i < 3; i++) { // rows
             PRAGMA_UNROLL
             for (uint32_t j = 0; j < 3; j++) { // cols
-                covars_c[i * 3 + j] = covar_c[j][i];
+                covars_c[i * 3 + j] = T(covar_c[j][i]);
             }
         }
     }
@@ -86,10 +92,12 @@ world_to_cam_fwd_tensor(const torch::Tensor &means,   // [N, 3]
 
     if (C && N) {
         at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
-        world_to_cam_fwd_kernel<float><<<(C * N + N_THREADS - 1) / N_THREADS, N_THREADS, 0, stream>>>(
-            C, N, means.data_ptr<float>(), covars.data_ptr<float>(),
-            viewmats.data_ptr<float>(), means_c.data_ptr<float>(),
-            covars_c.data_ptr<float>());
+        AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, means.scalar_type(), "world_to_cam_bwd", [&]() {
+            world_to_cam_fwd_kernel<scalar_t><<<(C * N + N_THREADS - 1) / N_THREADS, N_THREADS, 0, stream>>>(
+                C, N, means.data_ptr<scalar_t>(), covars.data_ptr<scalar_t>(),
+                viewmats.data_ptr<scalar_t>(), means_c.data_ptr<scalar_t>(),
+                covars_c.data_ptr<scalar_t>());
+        });
     }
     return std::make_tuple(means_c, covars_c);
 }
