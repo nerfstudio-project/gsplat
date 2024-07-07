3dec33cc6833ecf4a40557e62a8687f3884d5d10
Francis Williams <francis@fwilliams.info>
2024-07-07 13:17:40 -0400

    world to cam bwd

--- gsplat/cuda/csrc/world_to_cam_bwd.cu
+++ gsplat/cuda/csrc/world_to_cam_bwd.cu
@@ -27,8 +27,12 @@ world_to_cam_bwd_kernel(const uint32_t C, const uint32_t N,
                         T *__restrict__ v_covars,         // [N, 3, 3]
                         T *__restrict__ v_viewmats        // [C, 4, 4]
 ) {
+
+    // For now we'll upcast float16 and bfloat16 to float32
+    using OpT = typename OpType<T>::type;
+
     // parallelize over C * N.
-    uint32_t idx = cg::this_grid().thread_rank();
+    const uint32_t idx = cg::this_grid().thread_rank();
     if (idx >= C * N) {
         return;
     }
@@ -41,24 +45,27 @@ world_to_cam_bwd_kernel(const uint32_t C, const uint32_t N,
     viewmats += cid * 16;
 
     // glm is column-major but input is row-major
-    mat3<T> R = mat3<T>(viewmats[0], viewmats[4], viewmats[8], // 1st column
-                            viewmats[1], viewmats[5], viewmats[9], // 2nd column
-                            viewmats[2], viewmats[6], viewmats[10] // 3rd column
+    const mat3<OpT> R = mat3<OpT>(viewmats[0], viewmats[4], viewmats[8], // 1st column
+                                  viewmats[1], viewmats[5], viewmats[9], // 2nd column
+                                  viewmats[2], viewmats[6], viewmats[10] // 3rd column
     );
-    vec3<T> t = vec3<T>(viewmats[3], viewmats[7], viewmats[11]);
+    const vec3<OpT> t = vec3<OpT>(viewmats[3], viewmats[7], viewmats[11]);
 
-    vec3<T> v_mean(0.f);
-    mat3<T> v_covar(0.f);
-    mat3<T> v_R(0.f);
-    vec3<T> v_t(0.f);
+    vec3<OpT> v_mean(0.f);
+    mat3<OpT> v_covar(0.f);
+    mat3<OpT> v_R(0.f);
+    vec3<OpT> v_t(0.f);
 
     if (v_means_c != nullptr) {
-        vec3<T> v_mean_c = glm::make_vec3(v_means_c + idx * 3);
-        pos_world_to_cam_vjp<T>(R, t, glm::make_vec3(means), v_mean_c, v_R, v_t, v_mean);
+        const vec3<OpT> v_mean_c = glm::make_vec3(v_means_c + idx * 3);
+        const vec3<OpT> mean = glm::make_vec3(means);
+        pos_world_to_cam_vjp<OpT>(R, t, mean, v_mean_c, v_R, v_t, v_mean);
     }
     if (v_covars_c != nullptr) {
-        mat3<T> v_covar_c = glm::transpose(glm::make_mat3(v_covars_c + idx * 9));
-        covar_world_to_cam_vjp<T>(R, glm::make_mat3(covars), v_covar_c, v_R, v_covar);
+        const mat3<OpT> v_covar_c_t = glm::make_mat3(v_covars_c + idx * 9);
+        const mat3<OpT> v_covar_c = glm::transpose(v_covar_c_t);
+        const mat3<OpT> covar = glm::make_mat3(covars);
+        covar_world_to_cam_vjp<OpT>(R, covar, v_covar_c, v_R, v_covar);
     }
 
     // #if __CUDA_ARCH__ >= 700
@@ -83,7 +90,7 @@ world_to_cam_bwd_kernel(const uint32_t C, const uint32_t N,
             for (uint32_t i = 0; i < 3; i++) { // rows
                 PRAGMA_UNROLL
                 for (uint32_t j = 0; j < 3; j++) { // cols
-                    gpuAtomicAdd(v_covars + i * 3 + j, v_covar[j][i]);
+                    gpuAtomicAdd(v_covars + i * 3 + j, T(v_covar[j][i]));
                 }
             }
         }
@@ -98,9 +105,9 @@ world_to_cam_bwd_kernel(const uint32_t C, const uint32_t N,
             for (uint32_t i = 0; i < 3; i++) { // rows
                 PRAGMA_UNROLL
                 for (uint32_t j = 0; j < 3; j++) { // cols
-                    gpuAtomicAdd(v_viewmats + i * 4 + j, v_R[j][i]);
+                    gpuAtomicAdd(v_viewmats + i * 4 + j, T(v_R[j][i]));
                 }
-                gpuAtomicAdd(v_viewmats + i * 4 + 3, v_t[i]);
+                gpuAtomicAdd(v_viewmats + i * 4 + 3, T(v_t[i]));
             }
         }
     }
@@ -141,14 +148,16 @@ world_to_cam_bwd_tensor(const torch::Tensor &means,                    // [N, 3]
 
     if (C && N) {
         at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
-        world_to_cam_bwd_kernel<float><<<(C * N + N_THREADS - 1) / N_THREADS, N_THREADS, 0, stream>>>(
-            C, N, means.data_ptr<float>(), covars.data_ptr<float>(),
-            viewmats.data_ptr<float>(),
-            v_means_c.has_value() ? v_means_c.value().data_ptr<float>() : nullptr,
-            v_covars_c.has_value() ? v_covars_c.value().data_ptr<float>() : nullptr,
-            means_requires_grad ? v_means.data_ptr<float>() : nullptr,
-            covars_requires_grad ? v_covars.data_ptr<float>() : nullptr,
-            viewmats_requires_grad ? v_viewmats.data_ptr<float>() : nullptr);
+        AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, means.scalar_type(), "world_to_cam_bwd", [&]() {
+            world_to_cam_bwd_kernel<scalar_t><<<(C * N + N_THREADS - 1) / N_THREADS, N_THREADS, 0, stream>>>(
+                C, N, means.data_ptr<scalar_t>(), covars.data_ptr<scalar_t>(),
+                viewmats.data_ptr<scalar_t>(),
+                v_means_c.has_value() ? v_means_c.value().data_ptr<scalar_t>() : nullptr,
+                v_covars_c.has_value() ? v_covars_c.value().data_ptr<scalar_t>() : nullptr,
+                means_requires_grad ? v_means.data_ptr<scalar_t>() : nullptr,
+                covars_requires_grad ? v_covars.data_ptr<scalar_t>() : nullptr,
+                viewmats_requires_grad ? v_viewmats.data_ptr<scalar_t>() : nullptr);
+        });
     }
     return std::make_tuple(v_means, v_covars, v_viewmats);
 }
