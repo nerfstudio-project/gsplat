57522617e1f3a3b67c8099143c6a61ee7b57ddf5
Jeffrey Hu <hujh14@gmail.com>
2024-07-08 09:58:04 -0700

    benchmark script

--- examples/benchmark_mcmc.sh
+++ examples/benchmark_mcmc.sh
@@ -22,36 +22,6 @@ do
     echo "Running $SCENE"
     EVAL_STEPS="1000 7000 15000 30000"
 
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 2dgs_inria \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/2dgs_inria/$SCENE/
-
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 3dgs_inria \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/3dgs_inria/$SCENE/
-
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 3dgs \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/3dgs/$SCENE/
-
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 2dgs_inria \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --normal_consistency_loss \
-    #     --dist_loss \
-    #     --result_dir results/2dgs_inria_with_normal/$SCENE/
-
     python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
         --model_type 3dgs \
         --init_type sfm \
@@ -59,5 +29,34 @@ do
         --data_dir data/360_v2/$SCENE/ \
         --normal_consistency_loss \
         --result_dir results/3dgs_with_normal/$SCENE/
+
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 2dgs_inria \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --normal_consistency_loss \
+        --result_dir results/2dgs_inria_with_normal/$SCENE/
+
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 3dgs \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/3dgs/$SCENE/
+
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 2dgs_inria \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/2dgs_inria/$SCENE/
+
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 3dgs_inria \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/3dgs_inria/$SCENE/
 
 done
