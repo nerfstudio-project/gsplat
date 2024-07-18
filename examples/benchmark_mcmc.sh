db2c45c74f98f3c78b9615751a15eb37024d27d3
Jeffrey Hu <hujh14@gmail.com>
2024-07-18 10:05:34 -0700

    benchmark script

--- examples/benchmark_mcmc.sh
+++ examples/benchmark_mcmc.sh
@@ -1,14 +1,17 @@
 
 
-SCENE_FOLDER="data/360_v2"
+SCENE_DIR="data/360_v2"
+RESULTS_DIR="results/360_v2"
 SCENE_LIST="garden bicycle stump treehill flowers bonsai counter kitchen room"
 RENDER_TRAJ_PATH="interp"
 
-# SCENE_FOLDER="data/bilarf/bilarf_data/testscenes"
+# SCENE_DIR="data/bilarf/bilarf_data/testscenes"
+# RESULTS_DIR="results/bilarf"
 # SCENE_LIST="chinesearch lionpavilion pondbike statue strat building"
 # RENDER_TRAJ_PATH="spiral"
 
-# SCENE_FOLDER="data/bilarf/bilarf_data/editscenes"
+# SCENE_DIR="data/bilarf/bilarf_data/editscenes"
+# RESULTS_DIR="results/bilarf"
 # SCENE_LIST="rawnerf_windowlegovary rawnerf_sharpshadow scibldg"
 # RENDER_TRAJ_PATH="spiral"
 
@@ -27,21 +30,21 @@ do
     EVAL_STEPS="1000 7000 15000 30000"
     SAVE_STEPS="15000 30000"
 
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --save_steps $SAVE_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --max_steps $MAX_STEPS \
-    #     --data_dir $SCENE_FOLDER/$SCENE/ \
-    #     --render_traj_path $RENDER_TRAJ_PATH \
-    #     --result_dir results/3dgs/$SCENE/
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --save_steps $SAVE_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --max_steps $MAX_STEPS \
+        --data_dir $SCENE_DIR/$SCENE/ \
+        --render_traj_path $RENDER_TRAJ_PATH \
+        --result_dir $RESULTS_DIR/3dgs/$SCENE/
 
     python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --save_steps $SAVE_STEPS --disable_viewer --data_factor $DATA_FACTOR \
         --init_type sfm \
         --cap_max $CAP_MAX \
         --max_steps $MAX_STEPS \
-        --data_dir $SCENE_FOLDER/$SCENE/ \
+        --data_dir $SCENE_DIR/$SCENE/ \
         --render_traj_path $RENDER_TRAJ_PATH \
         --exp_opt \
-        --result_dir results/3dgs_exposure_opt/$SCENE/
+        --result_dir $RESULTS_DIR/3dgs_exposure_opt/$SCENE/
 
 done
