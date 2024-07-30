9c8be6f350a18cbb8080b69cd3fe02657dd26674
Jeffrey Hu <hujh14@gmail.com>
2024-07-30 15:50:24 -0700

    kwargs

--- gsplat/compression/compress.py
+++ gsplat/compression/compress.py
@@ -9,37 +9,70 @@ import numpy as np
 import imageio
 
 from plas import sort_with_plas
+from gsplat.utils import sh_to_rgb, rgb_to_sh, log_transform, inverse_log_transform
 
 
 def compress_splats(
-    compress_dir: str, splats: dict[str, Tensor], sort_plas: bool = True
+    compress_dir: str,
+    splats: dict[str, Tensor],
+    use_sort: bool = False,
+    use_kmeans: bool = True,
 ) -> None:
-    if sort_plas:
-        splats = sort_splats_with_plas(splats)
+    """Compress splats with sorting, quantization, and optionally, K-means clustering.
+
+    Args:
+        compress_dir (str): compression directory
+        splats (dict[str, Tensor]): splats
+        use_sort (bool, optional): Whether to sort splats before compression. Defaults to True.
+        use_kmeans (bool, optional): Whether to use K-means to compress spherical harmonics. Defaults to True.
+    """
+    if use_sort:
+        splats = sort_splats(splats)
 
     meta = {}
-    for attr_name in splats.keys():
-        compress_fn = eval(f"_compress_{attr_name}")
-        meta[attr_name] = compress_fn(compress_dir, splats[attr_name])
+    for param_name in splats.keys():
+        compress_fn = eval(f"_compress_{param_name}")
+        compress_kwargs = {}
+        if param_name == "shN":
+            compress_kwargs["use_kmeans"] = use_kmeans
+        meta[param_name] = compress_fn(
+            compress_dir, splats[param_name], **compress_kwargs
+        )
 
     with open(os.path.join(compress_dir, "meta.json"), "w") as f:
         json.dump(meta, f)
 
 
 def decompress_splats(compress_dir: str) -> dict[str, Tensor]:
+    """Decompress splats from directory.
+
+    Args:
+        compress_dir (str): compression directory
+
+    Returns:
+        dict[str, Tensor]: splats
+    """
     with open(os.path.join(compress_dir, "meta.json"), "r") as f:
         meta = json.load(f)
 
     splats = {}
-    for attr_name, attr_meta in meta.items():
-        decompress_fn = eval(f"_decompress_{attr_name}")
-        splats[attr_name] = decompress_fn(compress_dir, attr_meta)
+    for param_name, param_meta in meta.items():
+        decompress_fn = eval(f"_decompress_{param_name}")
+        splats[param_name] = decompress_fn(compress_dir, param_meta)
     return splats
 
 
-def sort_splats_with_plas(
-    splats: dict[str, Tensor], verbose: bool = True
-) -> dict[str, Tensor]:
+def sort_splats(splats: dict[str, Tensor], verbose: bool = False) -> dict[str, Tensor]:
+    """Sort splats with Parallel Linear Assignment Sorting from the paper `Compact 3D Scene Representation via
+    Self-Organizing Gaussian Grids <https://arxiv.org/pdf/2312.13299>`_.
+
+    Args:
+        splats (dict[str, Tensor]): splats
+        verbose (bool, optional): Whether to print verbose information. Default to False.
+
+    Returns:
+        dict[str, Tensor]: sorted splats
+    """
     n_gs = len(splats["means"])
     n_sidelen = int(n_gs**0.5)
     assert n_sidelen**2 == n_gs, "Must be a perfect square"
@@ -62,19 +95,15 @@ def sort_splats_with_plas(
 
 
 def _compress_means(compress_dir: str, params: Tensor) -> dict[str, Any]:
-    def log_transform(x):
-        return torch.sign(x) * torch.log1p(torch.abs(x))
-
-    params = log_transform(params)
-
     n_gs = len(params)
     n_sidelen = int(n_gs**0.5)
     assert n_sidelen**2 == n_gs, "Must be a perfect square"
 
+    params = log_transform(params)
     grid = params.reshape((n_sidelen, n_sidelen, -1))
-    grid_mins = torch.amin(grid, dim=(0, 1))
-    grid_maxs = torch.amax(grid, dim=(0, 1))
-    grid_norm = (grid - grid_mins) / (grid_maxs - grid_mins)
+    mins = torch.amin(grid, dim=(0, 1))
+    maxs = torch.amax(grid, dim=(0, 1))
+    grid_norm = (grid - mins) / (maxs - mins)
     img_norm = grid_norm.detach().cpu().numpy()
     img = (img_norm * (2**16 - 1)).round().astype(np.uint16)
 
@@ -85,25 +114,22 @@ def _compress_means(compress_dir: str, params: Tensor) -> dict[str, Any]:
 
     meta = {
         "shape": list(params.shape),
-        "mins": grid_mins.tolist(),
-        "maxs": grid_maxs.tolist(),
+        "mins": mins.tolist(),
+        "maxs": maxs.tolist(),
     }
     return meta
 
 
 def _decompress_means(compress_dir: str, meta: dict[str, Any]) -> Tensor:
-    def inverse_log_transform(y):
-        return torch.sign(y) * (torch.expm1(torch.abs(y)))
-
     img_l = imageio.imread(os.path.join(compress_dir, "means_l.png")).astype(np.uint16)
     img_u = imageio.imread(os.path.join(compress_dir, "means_u.png")).astype(np.uint16)
     img = (img_u << 8) + img_l
 
     img_norm = img / (2**16 - 1)
     grid_norm = torch.tensor(img_norm, dtype=torch.float32)
-    grid_mins = torch.tensor(meta["mins"], dtype=torch.float32)
-    grid_maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
-    grid = grid_norm * (grid_maxs - grid_mins) + grid_mins
+    mins = torch.tensor(meta["mins"], dtype=torch.float32)
+    maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
+    grid = grid_norm * (maxs - mins) + mins
 
     params = grid.reshape(meta["shape"])
     params = inverse_log_transform(params)
@@ -111,12 +137,14 @@ def _decompress_means(compress_dir: str, meta: dict[str, Any]) -> Tensor:
 
 
 def _compress_scales(compress_dir: str, params: Tensor) -> dict[str, Any]:
-    n_sidelen = int(params.shape[0] ** 0.5)
-    grid = params.reshape((n_sidelen, n_sidelen, -1))
+    n_gs = len(params)
+    n_sidelen = int(n_gs**0.5)
+    assert n_sidelen**2 == n_gs, "Must be a perfect square"
 
-    grid_mins = torch.amin(grid, dim=(0, 1))
-    grid_maxs = torch.amax(grid, dim=(0, 1))
-    grid_norm = (grid - grid_mins) / (grid_maxs - grid_mins)
+    grid = params.reshape((n_sidelen, n_sidelen, -1))
+    mins = torch.amin(grid, dim=(0, 1))
+    maxs = torch.amax(grid, dim=(0, 1))
+    grid_norm = (grid - mins) / (maxs - mins)
     img_norm = grid_norm.detach().cpu().numpy()
 
     img = (img_norm * (2**8 - 1)).round().astype(np.uint8)
@@ -124,8 +152,8 @@ def _compress_scales(compress_dir: str, params: Tensor) -> dict[str, Any]:
 
     meta = {
         "shape": list(params.shape),
-        "mins": grid_mins.tolist(),
-        "maxs": grid_maxs.tolist(),
+        "mins": mins.tolist(),
+        "maxs": maxs.tolist(),
     }
     return meta
 
@@ -135,22 +163,82 @@ def _decompress_scales(compress_dir: str, meta: dict[str, Any]) -> Tensor:
     img_norm = img / (2**8 - 1)
 
     grid_norm = torch.tensor(img_norm, dtype=torch.float32)
-    grid_mins = torch.tensor(meta["mins"], dtype=torch.float32)
-    grid_maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
-    grid = grid_norm * (grid_maxs - grid_mins) + grid_mins
+    mins = torch.tensor(meta["mins"], dtype=torch.float32)
+    maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
+    grid = grid_norm * (maxs - mins) + mins
+
+    params = grid.reshape(meta["shape"])
+    return params
+
+
+def _compress_quats(compress_dir: str, params: Tensor) -> Tensor:
+    n_gs = len(params)
+    n_sidelen = int(n_gs**0.5)
+    assert n_sidelen**2 == n_gs, "Must be a perfect square"
+
+    grid = params.reshape((n_sidelen, n_sidelen, -1))
+    grid_norm = F.normalize(grid, dim=-1)
+    grid_norm = grid_norm * 0.5 + 0.5
+    img_norm = grid_norm.detach().cpu().numpy()
+    img = (img_norm * (2**8 - 1)).round().astype(np.uint8)
+    imageio.imwrite(os.path.join(compress_dir, "quats.png"), img)
+
+    meta = {
+        "shape": list(params.shape),
+    }
+    return meta
+
+
+def _decompress_quats(compress_dir: str, meta: dict[str, Any]) -> Tensor:
+    img = imageio.imread(os.path.join(compress_dir, "quats.png"))
+    img_norm = img / (2**8 - 1)
+    grid_norm = torch.tensor(img_norm, dtype=torch.float32)
+    grid = grid_norm * 2 - 1
+
+    params = grid.reshape(meta["shape"])
+    return params
+
+
+def _compress_opacities(compress_dir: str, params: Tensor) -> Tensor:
+    n_gs = len(params)
+    n_sidelen = int(n_gs**0.5)
+    assert n_sidelen**2 == n_gs, "Must be a perfect square"
+
+    grid = params.reshape((n_sidelen, n_sidelen))
+    mins = torch.amin(grid, dim=(0, 1))
+    maxs = torch.amax(grid, dim=(0, 1))
+    grid_norm = (grid - mins) / (maxs - mins)
+    img_norm = grid_norm.detach().cpu().numpy()
+    img = (img_norm * (2**8 - 1)).round().astype(np.uint8)
+    imageio.imwrite(os.path.join(compress_dir, "opacities.png"), img)
+
+    meta = {
+        "shape": list(params.shape),
+        "mins": mins.tolist(),
+        "maxs": maxs.tolist(),
+    }
+    return meta
+
+
+def _decompress_opacities(compress_dir: str, meta: dict[str, Any]) -> Tensor:
+    img = imageio.imread(os.path.join(compress_dir, "opacities.png"))
+    img_norm = img / (2**8 - 1)
+    grid_norm = torch.tensor(img_norm, dtype=torch.float32)
+    mins = torch.tensor(meta["mins"], dtype=torch.float32)
+    maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
+    grid = grid_norm * (maxs - mins) + mins
 
     params = grid.reshape(meta["shape"])
     return params
 
 
 def _compress_sh0(compress_dir: str, params: Tensor) -> Tensor:
-    def sh_to_rgb(sh: Tensor) -> Tensor:
-        C0 = 0.28209479177387814
-        return sh * C0 + 0.5
+    n_gs = len(params)
+    n_sidelen = int(n_gs**0.5)
+    assert n_sidelen**2 == n_gs, "Must be a perfect square"
 
     rgb = sh_to_rgb(params)
     rgb = torch.clamp(rgb, 0.0, 1.0)
-    n_sidelen = int(params.shape[0] ** 0.5)
     grid = rgb.reshape((n_sidelen, n_sidelen, -1))
     grid = grid.detach().cpu().numpy()
     img = (grid * (2**8 - 1)).round().astype(np.uint8)
@@ -163,10 +251,6 @@ def _compress_sh0(compress_dir: str, params: Tensor) -> Tensor:
 
 
 def _decompress_sh0(compress_dir: str, meta: dict[str, Any]) -> Tensor:
-    def rgb_to_sh(rgb: Tensor) -> Tensor:
-        C0 = 0.28209479177387814
-        return (rgb - 0.5) / C0
-
     img = imageio.imread(os.path.join(compress_dir, "sh0.png"))
     grid = img / (2**8 - 1)
     grid = torch.tensor(grid, dtype=torch.float32)
@@ -175,9 +259,7 @@ def _decompress_sh0(compress_dir: str, meta: dict[str, Any]) -> Tensor:
     return params
 
 
-def _compress_shN(
-    compress_dir: str, params: Tensor, use_kmeans: bool = False
-) -> Tensor:
+def _compress_shN(compress_dir: str, params: Tensor, use_kmeans: bool = True) -> Tensor:
     if params.numel() == 0:
         return {"shape": list(params.shape)}
 
@@ -210,7 +292,7 @@ def _compress_shN(
 
 
 def _decompress_shN(
-    compress_dir: str, meta: dict[str, Any], use_kmeans: bool = False
+    compress_dir: str, meta: dict[str, Any], use_kmeans: bool = True
 ) -> Tensor:
     if meta["shape"][1] == 0:
         return torch.zeros(meta["shape"], dtype=torch.float32)
@@ -228,100 +310,4 @@ def _decompress_shN(
         labels = npz_dict["labels"]
         codebook = codebook[labels]
     params = codebook.reshape(meta["shape"])
-    return params
-
-
-def _compress_shN_codebook(
-    compress_dir: str, centroids: Tensor, labels: Tensor
-) -> Tensor:
-    centroids_mins = torch.min(centroids)
-    centroid_maxs = torch.max(centroids)
-    centroids_norm = (centroids - centroids_mins) / (centroid_maxs - centroids_mins)
-
-    labels = labels.detach().cpu().numpy()
-    centroids_norm = centroids_norm.detach().cpu().numpy()
-    labels = labels.astype(np.uint16)
-    centroids = (centroids_norm * (2**16 - 1)).round().astype(np.uint16)
-    np.savez_compressed(
-        os.path.join(compress_dir, "shN.npz"), labels=labels, centroids=centroids
-    )
-
-    meta = {
-        # "shape": list(params.shape),
-        "mins": centroids_mins.tolist(),
-        "maxs": centroid_maxs.tolist(),
-    }
-    return meta
-
-
-def _decompress_shN_codebook(compress_dir: str, meta: dict[str, Any]) -> Tensor:
-    npz_dict = np.load(os.path.join(compress_dir, "shN.npz"))
-    labels = npz_dict["labels"]
-    centroids = npz_dict["centroids"]
-
-    centroids_norm = centroids / (2**16 - 1)
-    centroids_norm = torch.tensor(centroids_norm, dtype=torch.float32)
-    centroids_mins = torch.tensor(meta["mins"], dtype=torch.float32)
-    centroids_maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
-    centroids = centroids_norm * (centroids_maxs - centroids_mins) + centroids_mins
-
-    params = centroids[labels]
-    # params = params.reshape(meta["shape"])
-    return params
-
-
-def _compress_quats(compress_dir: str, params: Tensor) -> Tensor:
-    n_sidelen = int(params.shape[0] ** 0.5)
-    grid = params.reshape((n_sidelen, n_sidelen, -1))
-
-    grid_norm = F.normalize(grid, dim=-1)
-    grid_norm = grid_norm * 0.5 + 0.5
-    img_norm = grid_norm.detach().cpu().numpy()
-    img = (img_norm * (2**8 - 1)).round().astype(np.uint8)
-    imageio.imwrite(os.path.join(compress_dir, "quats.png"), img)
-
-    meta = {
-        "shape": list(params.shape),
-    }
-    return meta
-
-
-def _decompress_quats(compress_dir: str, meta: dict[str, Any]) -> Tensor:
-    img = imageio.imread(os.path.join(compress_dir, "quats.png"))
-    img_norm = img / (2**8 - 1)
-    grid_norm = torch.tensor(img_norm, dtype=torch.float32)
-    grid = grid_norm * 2 - 1
-
-    params = grid.reshape(meta["shape"])
-    return params
-
-
-def _compress_opacities(compress_dir: str, params: Tensor) -> Tensor:
-    n_sidelen = int(params.shape[0] ** 0.5)
-    grid = params.reshape((n_sidelen, n_sidelen))
-
-    grid_mins = torch.amin(grid, dim=(0, 1))
-    grid_maxs = torch.amax(grid, dim=(0, 1))
-    grid_norm = (grid - grid_mins) / (grid_maxs - grid_mins)
-    img_norm = grid_norm.detach().cpu().numpy()
-    img = (img_norm * (2**8 - 1)).round().astype(np.uint8)
-    imageio.imwrite(os.path.join(compress_dir, "opacities.png"), img)
-
-    meta = {
-        "shape": list(params.shape),
-        "mins": grid_mins.tolist(),
-        "maxs": grid_maxs.tolist(),
-    }
-    return meta
-
-
-def _decompress_opacities(compress_dir: str, meta: dict[str, Any]) -> Tensor:
-    img = imageio.imread(os.path.join(compress_dir, "opacities.png"))
-    img_norm = img / (2**8 - 1)
-    grid_norm = torch.tensor(img_norm, dtype=torch.float32)
-    grid_mins = torch.tensor(meta["mins"], dtype=torch.float32)
-    grid_maxs = torch.tensor(meta["maxs"], dtype=torch.float32)
-    grid = grid_norm * (grid_maxs - grid_mins) + grid_mins
-
-    params = grid.reshape(meta["shape"])
     return params
