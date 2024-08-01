7edee580dfe87f2550851abf6ab826f5449d0532
Jeffrey Hu <hujh14@gmail.com>
2024-07-31 21:20:14 -0700

    utils

--- examples/utils.py
+++ examples/utils.py
@@ -143,12 +143,12 @@ def knn(x: Tensor, K: int = 4) -> Tensor:
     return torch.from_numpy(distances).to(x)
 
 
+def rgb_to_sh(rgb: Tensor) -> Tensor:
+    C0 = 0.28209479177387814
+    return (rgb - 0.5) / C0
+
+
 def set_random_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-
-
-def rgb_to_sh(rgb: Tensor) -> Tensor:
-    C0 = 0.28209479177387814
-    return (rgb - 0.5) / C0
