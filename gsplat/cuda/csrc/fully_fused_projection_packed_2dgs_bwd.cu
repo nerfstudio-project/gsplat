c79a32e3d72c4195b189839805470a6a1ec2152c
FantasticOven2 <0z5e1n3g@berkeley.edu>
2024-08-07 10:14:29 -0700

    all tests passed

--- gsplat/cuda/csrc/fully_fused_projection_packed_2dgs_bwd.cu
+++ gsplat/cuda/csrc/fully_fused_projection_packed_2dgs_bwd.cu
@@ -84,7 +84,7 @@ __global__ void fully_fused_projection_packed_bwd_2dgs_kernel(
     vec3<T> v_normal = glm::make_vec3(v_normals);
 
     vec3<T> v_mean(0.f);
-    vec3<T> v_scale(0.f);
+    vec2<T> v_scale(0.f);
     vec4<T> v_quat(0.f);
     compute_ray_Ms_aabb_vjp<T>(
         ray_Ms,
