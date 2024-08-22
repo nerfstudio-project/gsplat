f98defd01f6f6c75aaf6fa4c21a6121244856804
Martin de La Gorce <madelago@microsoft.com>
2024-08-22 19:17:58 +0100

    test TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6" with cuda 11.3

--- .github/workflows/cuda/Windows-env.sh
+++ .github/workflows/cuda/Windows-env.sh
@@ -19,19 +19,19 @@ case ${1} in
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.6
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu115)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu113)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   *)
     ;;
