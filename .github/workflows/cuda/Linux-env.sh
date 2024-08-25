ff3c3602f93b59ae8876eefd1c33d45826c0bc7f
Ruilong Li <397653553@qq.com>
2024-08-25 23:31:20 +0000

    update building workflow

--- .github/workflows/cuda/Linux-env.sh
+++ .github/workflows/cuda/Linux-env.sh
@@ -7,37 +7,37 @@ case ${1} in
     export CUDA_HOME=/usr/local/cuda-11.8
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu117)
     export CUDA_HOME=/usr/local/cuda-11.7
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu116)
     export CUDA_HOME=/usr/local/cuda-11.6
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu115)
     export CUDA_HOME=/usr/local/cuda-11.5
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu113)
     export CUDA_HOME=/usr/local/cuda-11.3
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu102)
     export CUDA_HOME=/usr/local/cuda-10.2
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5"
     ;;
   *)
     ;;
--- .github/workflows/cuda/Windows-env.sh
+++ .github/workflows/cuda/Windows-env.sh
@@ -7,31 +7,31 @@ case ${1} in
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.8
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu117)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.7
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu116)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.6
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu115)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   cu113)
     CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3
     PATH=${CUDA_HOME}/bin:$PATH
     PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
-    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6"
     ;;
   *)
     ;;
