a014c893daf2192e5633359a63ba70644729ad53
Jeffrey Hu <hujh14@gmail.com>
2024-08-31 13:23:05 -0700

    uncomment

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -923,7 +923,7 @@ def main(local_rank: int, world_rank, world_size: int, cfg: Config):
             runner.splats[k].data = torch.cat([ckpt["splats"][k] for ckpt in ckpts])
         step = ckpts[0]["step"]
         runner.eval(step=step)
-        # runner.render_traj(step=step)
+        runner.render_traj(step=step)
         if cfg.compression is not None:
             runner.run_compression(step=step)
     else:
