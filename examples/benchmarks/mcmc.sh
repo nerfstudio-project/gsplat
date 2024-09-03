57c77b9aa3b3df03093416299b0399ae739af3d5
Jeffrey Hu <hujh14@gmail.com>
2024-09-03 11:45:34 -0700

    Add option to render camera trajectory as an ellipse. (#380)
    * ellipse
    
    * cleanup
    
    * uncomment
    
    * canvas list

--- examples/benchmarks/mcmc.sh
+++ examples/benchmarks/mcmc.sh
@@ -1,6 +1,7 @@
 SCENE_DIR="data/360_v2"
 RESULT_DIR="results/benchmark_mcmc_1M"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
+RENDER_TRAJ_PATH="ellipse"
 
 CAP_MAX=1000000
 
@@ -17,6 +18,7 @@ do
     # train without eval
     CUDA_VISIBLE_DEVICES=0 python simple_trainer.py mcmc --eval_steps -1 --disable_viewer --data_factor $DATA_FACTOR \
         --strategy.cap-max $CAP_MAX \
+        --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir data/360_v2/$SCENE/ \
         --result_dir $RESULT_DIR/$SCENE/
 
@@ -25,6 +27,7 @@ do
     do
         CUDA_VISIBLE_DEVICES=0 python simple_trainer.py mcmc --disable_viewer --data_factor $DATA_FACTOR \
             --strategy.cap-max $CAP_MAX \
+            --render_traj_path $RENDER_TRAJ_PATH \
             --data_dir $SCENE_DIR/$SCENE/ \
             --result_dir $RESULT_DIR/$SCENE/ \
             --ckpt $CKPT
