67baaae487997ccf343b0b406f768c6edb91af56
Jeffrey Hu <hujh14@gmail.com>
2024-09-10 12:53:16 -0700

    fix bug

--- examples/datasets/colmap.py
+++ examples/datasets/colmap.py
@@ -1,5 +1,6 @@
 import os
-from typing import Any, Dict, List, Optional, assert_never
+from typing import Any, Dict, List, Optional
+from typing_extensions import assert_never
 
 import cv2
 import imageio.v2 as imageio
