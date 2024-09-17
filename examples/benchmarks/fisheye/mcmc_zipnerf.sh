41aa3982b9cad8554ba4c5b42f63a7c76a306b12
Jeffrey Hu <hujh14@gmail.com>
2024-09-17 15:23:42 -0700

    ellipse

--- examples/benchmarks/fisheye/mcmc_zipnerf.sh
+++ examples/benchmarks/fisheye/mcmc_zipnerf.sh
@@ -1,6 +1,7 @@
 SCENE_DIR="data/zipnerf"
 SCENE_LIST="berlin london nyc alameda"
-DATA_FACTOR=2
+DATA_FACTOR=4
+RENDER_TRAJ_PATH="ellipse"
 
 RESULT_DIR="results/benchmark_mcmc_2M_zipnerf"
 CAP_MAX=2000000
@@ -16,7 +17,9 @@ do
     CUDA_VISIBLE_DEVICES=0 python simple_trainer.py mcmc --disable_viewer --data_factor $DATA_FACTOR \
         --strategy.cap-max $CAP_MAX \
         --opacity_reg 0.001 \
+        --render_traj_path $RENDER_TRAJ_PATH \
         --camera_model fisheye \
         --data_dir $SCENE_DIR/$SCENE/ \
         --result_dir $RESULT_DIR/$SCENE/
+
 done
