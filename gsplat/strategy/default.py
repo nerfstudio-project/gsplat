d333f88b19f92431144aa4940baca22882bdf10f
Janusch Patas <janusch.patas@gmx.de>
2025-01-30 08:20:27 +0000

    lower densification threshold

--- gsplat/strategy/default.py
+++ gsplat/strategy/default.py
@@ -77,7 +77,7 @@ class DefaultStrategy(Strategy):
     """
 
     prune_opa: float = 0.005
-    grow_grad2d: float = 0.0002
+    grow_grad2d: float = 0.00012
     grow_scale3d: float = 0.01
     grow_scale2d: float = 0.05
     prune_scale3d: float = 0.1
