ad1df17c4cd15bbfa38c5e22660d8b182a1bf61e
Ruilong Li <397653553@qq.com>
2025-04-04 17:12:09 +0000

    fix adam shape issue

--- gsplat/cuda/csrc/Adam.h
+++ gsplat/cuda/csrc/Adam.h
@@ -9,11 +9,11 @@ class Tensor;
 namespace gsplat {
 
 void launch_adam_kernel(
-    at::Tensor &param,                    // [..., D]
-    const at::Tensor &param_grad,         // [..., D]
-    at::Tensor &exp_avg,                  // [..., D]
-    at::Tensor &exp_avg_sq,               // [..., D]
-    const at::optional<at::Tensor> valid, // [...]
+    at::Tensor &param,                    // [N, ...]
+    const at::Tensor &param_grad,         // [N, ...]
+    at::Tensor &exp_avg,                  // [N, ...]
+    at::Tensor &exp_avg_sq,               // [N, ...]
+    const at::optional<at::Tensor> valid, // [N]
     const float lr,
     const float b1,
     const float b2,
