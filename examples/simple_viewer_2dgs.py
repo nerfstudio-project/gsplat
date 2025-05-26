738484540e0817e2e68ced15d01ac06251eb592f
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2025-05-25 23:10:36 -0700

    Create simple_viewer_2dgs.py

---
+++ examples/simple_viewer_2dgs.py
@@ -0,0 +1,157 @@
+import argparse
+import math
+import time
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import viser
+from pathlib import Path
+from gsplat.distributed import cli
+from gsplat.rendering import rasterization_2dgs
+
+from nerfview import CameraState, RenderTabState, apply_float_colormap
+from gsplat_viewer_2dgs import GsplatViewer, GsplatRenderTabState
+
+
+def main(local_rank: int, world_rank, world_size: int, args):
+    torch.manual_seed(42)
+    device = torch.device("cuda", local_rank)
+
+    means, quats, scales, opacities, sh0, shN = [], [], [], [], [], []
+    for ckpt_path in args.ckpt:
+        ckpt = torch.load(ckpt_path, map_location=device)["splats"]
+        means.append(ckpt["means"])
+        quats.append(F.normalize(ckpt["quats"], p=2, dim=-1))
+        scales.append(torch.exp(ckpt["scales"]))
+        opacities.append(torch.sigmoid(ckpt["opacities"]))
+        sh0.append(ckpt["sh0"])
+        shN.append(ckpt["shN"])
+    means = torch.cat(means, dim=0)
+    quats = torch.cat(quats, dim=0)
+    scales = torch.cat(scales, dim=0)
+    opacities = torch.cat(opacities, dim=0)
+    sh0 = torch.cat(sh0, dim=0)
+    shN = torch.cat(shN, dim=0)
+    colors = torch.cat([sh0, shN], dim=-2)
+    sh_degree = int(math.sqrt(colors.shape[-2]) - 1)
+    print("Number of Gaussians:", len(means))
+
+    # register and open viewer
+    @torch.no_grad()
+    def viewer_render_fn(camera_state: CameraState, render_tab_state: RenderTabState):
+        assert isinstance(render_tab_state, GsplatRenderTabState)
+        if render_tab_state.preview_render:
+            width = render_tab_state.render_width
+            height = render_tab_state.render_height
+        else:
+            width = render_tab_state.viewer_width
+            height = render_tab_state.viewer_height
+        c2w = camera_state.c2w
+        K = camera_state.get_K((width, height))
+        c2w = torch.from_numpy(c2w).float().to(device)
+        K = torch.from_numpy(K).float().to(device)
+        viewmat = c2w.inverse()
+
+        (
+            render_colors,
+            render_alphas,
+            render_normals,
+            normals_from_depth,
+            render_distort,
+            render_median,
+            info,
+        ) = rasterization_2dgs(
+            means=means,
+            quats=quats,
+            scales=scales,
+            opacities=opacities,
+            colors=colors,
+            viewmats=viewmat[None],
+            Ks=K[None],
+            width=width,
+            height=height,
+            sh_degree=(
+                min(render_tab_state.max_sh_degree, sh_degree)
+                if sh_degree is not None
+                else None
+            )
+        )
+        render_tab_state.total_gs_count = len(means)
+        render_tab_state.rendered_gs_count = (info["radii"] > 0).all(-1).sum().item()
+
+        if render_tab_state.render_mode == "depth":
+            # normalize depth to [0, 1]
+            depth = render_median
+            if render_tab_state.normalize_nearfar:
+                near_plane = render_tab_state.near_plane
+                far_plane = render_tab_state.far_plane
+            else:
+                near_plane = depth.min()
+                far_plane = depth.max()
+            depth_norm = (depth - near_plane) / (far_plane - near_plane + 1e-10)
+            depth_norm = torch.clip(depth_norm, 0, 1)
+            if render_tab_state.inverse:
+                depth_norm = 1 - depth_norm
+            renders = (
+                apply_float_colormap(depth_norm, render_tab_state.colormap)
+                .cpu()
+                .numpy()
+            )
+        elif render_tab_state.render_mode == "normal":
+            render_normals = render_normals * 0.5 + 0.5  # normalize to [0, 1]
+            renders = render_normals.cpu().numpy()
+        elif render_tab_state.render_mode == "alpha":
+            alpha = render_alphas[0, ..., 0:1]
+            renders = (
+                apply_float_colormap(alpha, render_tab_state.colormap).cpu().numpy()
+            )
+        else:
+            render_colors = render_colors[0, ..., 0:3].clamp(0, 1)
+            renders = render_colors.cpu().numpy()
+        return renders
+
+    server = viser.ViserServer(port=args.port, verbose=False)
+    _ = GsplatViewer(
+        server=server,
+        render_fn=viewer_render_fn,
+        output_dir=Path(args.output_dir),
+        mode="rendering",
+    )
+    print("Viewer running... Ctrl+C to exit.")
+    time.sleep(100000)
+
+
+if __name__ == "__main__":
+    """
+    # Use single GPU to view the scene
+    CUDA_VISIBLE_DEVICES=9 python -m simple_viewer \
+        --ckpt results/garden/ckpts/ckpt_6999_rank0.pt \
+        --output_dir results/garden/ \
+        --port 8082
+    
+    CUDA_VISIBLE_DEVICES=9 python -m simple_viewer \
+        --output_dir results/garden/ \
+        --port 8082
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_dir", type=str, default="results/", help="where to dump outputs"
+    )
+    parser.add_argument(
+        "--scene_grid", type=int, default=1, help="repeat the scene into a grid of NxN"
+    )
+    parser.add_argument(
+        "--ckpt", type=str, nargs="+", default=None, help="path to the .pt file"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="port for the viewer server"
+    )
+    parser.add_argument(
+        "--with_ut", action="store_true", help="use uncentered transform"
+    )
+    parser.add_argument("--with_eval3d", action="store_true", help="use eval 3D")
+    args = parser.parse_args()
+    assert args.scene_grid % 2 == 1, "scene_grid must be odd"
+
+    cli(main, args, verbose=True)
